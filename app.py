425ad9285040e423d416437f3af86c170c857b46
soufian <elhabib.soufian@gmail.com>
2022-05-07 09:31:32 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import shutil # to save it locally
+
+# Making a GET request
+r = requests.get('https://www.monaloc.ma/en/our-cars/2')
+
+
+# Parsing the HTML
+soup = BeautifulSoup(r.content, 'html.parser')
+
+
+for tr in soup.find_all('div',class_='featured-car-img'):
+
+    url = "URL: "
+    values = [data for data in tr.find_all('img',class_='img-responsive')]
+    for value in values:
+        
+        ## Set up the image URL and filename
+        image_url = value.get('src')
+        filename = image_url.split("/")[-1]
+
+        # Open the url image, set stream to True, this will return the stream content.
+        r = requests.get(image_url, stream = True)
+
+        # Check if the image was retrieved successfully
+        if r.status_code == 200:
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            r.raw.decode_content = True
+            
+            # Open a local file with wb ( write binary ) permission.
+            with open(filename,'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+                
+            print('Image sucessfully Downloaded: ',filename)
+        else:
+            print('Image Couldn\'t be retreived')
+    print()
